08c51ef14260b0131a46dc9550c0a8f3db0b231d
Robert Müller 🚀 <robert.mueller@hetzner-cloud.de>
2022-05-13 14:00:08 +0200

    fix typo
    Signed-off-by: Robert Müller 🚀 <robert.mueller@hetzner-cloud.de>

--- apps/hetzner/go/scripts/go.sh
+++ apps/hetzner/go/scripts/go.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Download given Go version and check sha256 checksum
-wget "https://go.dev/dl/go${application_version}.linux.amd64.tar.gz" -O /tmp/go.tar.gz
+wget "https://go.dev/dl/go${application_version}.linux-amd64.tar.gz" -O /tmp/go.tar.gz
 
 echo "${application_checksum} /tmp/go.tar.gz" | sha256sum -c
 
