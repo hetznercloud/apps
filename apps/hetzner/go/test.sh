9f1cbf3b97154db77662b96cbf1c4bd7906d51e9
Max Rosin <max.rosin@hetzner-cloud.de>
2022-05-24 13:23:03 +0200

    source .profile in go test.sh before setting -e

--- apps/hetzner/go/test.sh
+++ apps/hetzner/go/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -uexo pipefail
 
 # The bash in GitHub action is running non-interactive and therefore does not
 # source the .profile that extend the path for the go binary.
 # https://github.community/t/self-hosted-not-using-bashrc/18358
-
 source $HOME/.profile
+
+set -uexo pipefail
 
 go version || exit 1
