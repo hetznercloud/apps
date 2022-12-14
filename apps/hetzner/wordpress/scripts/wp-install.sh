45f4ffe1387de76e4b29e21bf2e33030337a09e6
Robert Müller <robert.mueller@hetzner-cloud.de>
2022-12-14 14:59:47 +0100

    [Wordpress] Use PHP 8.1
    Signed-off-by: Robert Müller <robert.mueller@hetzner-cloud.de>

--- apps/hetzner/wordpress/scripts/wp-install.sh
+++ apps/hetzner/wordpress/scripts/wp-install.sh
@@ -6,7 +6,7 @@ set -e
 sed -e "s|upload_max_filesize.*|upload_max_filesize = 50M|g" \
     -e "s|post_max_size.*|post_max_size = 50M|g" \
     -e "s|max_execution_time.*|max_execution_time = 60|g" \
-    -i /etc/php/7.4/apache2/php.ini
+    -i /etc/php/8.1/apache2/php.ini
 
 # Download given WordPress version and check sha256 checksum
 wget "https://wordpress.org/wordpress-${application_version}.tar.gz" -O /tmp/wordpress.tar.gz
